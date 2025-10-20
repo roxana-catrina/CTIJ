2ee9a36b64fcc1331e0b01359fc9692c338ae59e
roxana-catrina <roxanacatrina22@gmail.com>
2025-10-20 20:12:50 +0300

    collect all before key

--- Assets/KeyPickup.cs
+++ Assets/KeyPickup.cs
@@ -8,6 +8,7 @@ public class KeyPickup : MonoBehaviour
         // verificăm dacă obiectul care a intrat în trigger este player-ul
         if (other.CompareTag("Player"))
         {
+            if(CoinManager.instance.maps==1 && CoinManager.instance.potions==1 && CoinManager.instance.coinsCollected==2)  
             // trecem la scena Level 2
             SceneManager.LoadScene("Level 2");
         }
