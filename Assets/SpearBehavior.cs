8a9488648c31faea2879f5e3ac6ff45da31d7173
roxana-catrina <roxanacatrina22@gmail.com>
2025-10-28 21:24:55 +0200

    sageata fuge catre enemy

---
+++ Assets/SpearBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpearBehavior : MonoBehaviour
+{
+    public float lifetime = 5f; // Timp după care sulița se distruge automat
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            // Aici poți adăuga logică pentru damage
+            Debug.Log("Sulița a lovit inamicul!");
+
+            // Distruge sulița
+            Destroy(gameObject);
+
+            // Opțional: distruge și inamicul sau reduce-i viața
+            // Destroy(collision.gameObject);
+        }
+    }
+}
