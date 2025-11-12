181b2775e9e2917ef2d3d82af5482e37e3c400ef
Ionela Secman <ionellaccl@gmail.com>
2025-11-12 12:52:32 +0200

    am adaugat la start buton de quit

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,9 +9,21 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-       
 
-            SceneManager.LoadScene("Story");
-        
+
+        SceneManager.LoadScene("Story");
+
+    }
+    
+     public void QuitGame()
+    {
+        Debug.Log("Quit button pressed!"); // doar pentru test în editor
+        Application.Quit();
+
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
